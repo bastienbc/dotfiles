b67b319ad9fb5ee811e5135c74541aad2b3ff40c
Bastien Barbé Creuly <bastien.barbe-creuly-externe@acoss.fr>
2018-05-14 13:53:15 +0200

    Don't export variables

--- plugins/tpm.zsh
+++ plugins/tpm.zsh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
-BBC_REPOS_PATH=${0:A:h:h}
-
 has_tpm() {
 	[ -d "$HOME/.tmux/plugins/tpm" ]
 }
@@ -13,9 +11,9 @@ install_tpm() {
 }
 if [ ! -f "$HOME/.tmux.conf" ]
 then
-	ln -s "$BBC_REPOS_PATH/.tmux.conf" "$HOME/.tmux.conf" >& /dev/null
+	ln -s "${0:A:h:h}/.tmux.conf" "$HOME/.tmux.conf" >& /dev/null
 fi
 if [ ! -f "$HOME/.tmux-powerlinerc" ]
 then
-	ln -s "$BBC_REPOS_PATH/.tmux-powerlinerc" "$HOME/.tmux-powerlinerc" >& /dev/null
+	ln -s "${0:A:h:h}/.tmux-powerlinerc" "$HOME/.tmux-powerlinerc" >& /dev/null
 fi
