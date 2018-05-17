3748082ff5af4ad720b1bcba14f142ddb58f2651
Bastien Barbé Creuly <bastien.barbe-creuly-externe@acoss.fr>
2018-05-17 14:00:27 +0200

    Add Nerd font

--- plugins/powerline-symbols.zsh
+++ plugins/powerline-symbols.zsh
@@ -4,14 +4,18 @@
 install_powerline_symbols() {
 	if ! has_powerline_symbols
 	then
+		(
 		cd "$HOME/.local/share/fonts/"
-		wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+		wget "https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf"
+		wget "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.ttf"
 		fc-cache -vf $(pwd)
 		cd "$HOME/.config/fontconfig/conf.d/"
-		wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+		wget "https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf"
+		wget "https://github.com/ryanoasis/nerd-fonts/raw/master/10-nerd-font-symbols.conf"
+		)
 	fi
 }
 
 has_powerline_symbols() {
-	[ -f "$HOME/.config/fontconfig/conf.d/10-powerline-symbols.conf" ]
+	[ -f "$HOME/.config/fontconfig/conf.d/10-nerd-font-symbols.conf" ]
 }
--- plugins/purist.zsh
+++ plugins/purist.zsh
@@ -3,11 +3,25 @@
 zstyle ':vcs_info:git*+set-message:*' hooks getGitTag
 
 function +vi-getGitTag() {
+    hook_com[branch]=$'\ue725'" ${hook_com[branch]}"
 	local tag
-	tag="$(git describe --tags 2> /dev/null)"
+	tag="$(git describe --tags --exact-match 2> /dev/null)"
 	if [ $? -eq 0 ]; then
-		hook_com[branch]="${hook_com[branch]}${tag:+ <${tag}>}"
+		hook_com[branch]=$(echo "${hook_com[branch]}${tag:+ \uf73e ${tag}}")
+	else
+		local sha
+		sha="$(git rev-parse --verify --short HEAD 2> /dev/null)"
+		if [ $? -eq 0 ]; then
+			hook_com[branch]=$(echo "${hook_com[branch]}${sha:+ \ue729 ${sha}}")
+		fi
 	fi
+	local stash
+	stash="$(git stash list -q 2> /dev/null)"
+	if [ -n "$stash" ]
+	then
+		hook_com[branch]="${hook_com[branch]} $(wc -l <<< "$stash")"$'\ue78f'
+	fi
+	hook_com[branch]="${hook_com[branch]} "
 }
 
 prompt_pure_update_vim_prompt() {
@@ -31,7 +45,7 @@ zle -N zle-keymap-select
 #prompt_newline='%666v'
 RPROMPT='%F{white}%*'
 prompt_newline='${VIM_PROMPT:+ $VIM_PROMPT }'$'\n%{\r%}'
-PROMPT='%(1j.[%j] .)'
+PROMPT='%(1j.[%j\uf04c] .)'
 PROMPT+='%(12V.%F{242}%12v${PURE_PROMPT_SYMBOL:-❯}%f .)'
 PROMPT+='%(?.%F{cyan}.%F{red}${PURE_PROMPT_SYMBOL:-❯} $? %F{cyan})'
 PROMPT+='${PURE_PROMPT_SYMBOL:-❯}%f '
