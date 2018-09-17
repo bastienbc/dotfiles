68c61971ee1f0da240fdb31b9945e5b64569fb2d
Bastien Barbé Creuly <bastien.barbe-creuly-externe@acoss.fr>
2018-09-17 13:14:10 +0200

    Reduce loading time by not checking solarized xfwm4 theme

--- plugins/xfce-theme.zsh
+++ plugins/xfce-theme.zsh
@@ -1,25 +1,22 @@
 #!/usr/bin/env zsh
 
 install_xfce_theme() {
-	local REPO="$HOME/.local/repo/solarized-dark-xfce"
 	if ! has_xfce_theme
 	then
+		[ ! -d "$HOME/.local/share/themes" ] && mkdir -p "$HOME/.local/share/themes"
 		(
+			local REPO="$HOME/.local/repo/solarized-dark-xfce"
 			[ ! -d "$REPO" ] && mkdir -p "$REPO"
-			git clone https://github.com/mzgnr/solarized-dark-xfce.git "$REPO"
-			[ ! -d "$HOME/.local/share/themes" ] && mkdir -p "$HOME/.local/share/themes"
+			git clone https://github.com/mzgnr/solarized-dark-xfce.git "$REPO" >& /dev/null
 			ln -fs "$REPO/xfce-theme/Numix Solarized" "$HOME/.local/share/themes/Numix Solarized"
 			[ ! -d "$HOME/.config/xfce4/terminal" ] && mkdir -p "$HOME/.config/xfce4/terminal"
 			ln -fs "$REPO/xfce-terminal/terminalrc" "$HOME/.config/xfce4/terminal/terminalrc"
-			xfconf-query -c xsettings -p /Net/ThemeName -s "Numix Solarized"
             xfconf-query -c xfwm4 -p /general/theme -s "Numix Solarized"
 		)
 	fi
 }
 
 has_xfce_theme() {
-	local REPO="$HOME/.local/repo/solarized-dark-xfce"
-	git -C "$REPO" fetch >& /dev/null
-	[ -d "$REPO" ] && ( [ "$(git -C "$REPO" rev-parse HEAD)" = "$(git -C "$REPO" rev-parse @{u})" ] || git -C "$REPO" pull --rebase )
-	[ -d "$HOME/.local/share/themes/Numix Solarized" ] && [ -f "$HOME/.config/xfce4/terminal/terminalrc" ]
+	[ -d "$HOME/.local/share/themes/Numix Solarized" ] \
+		&& [ -f "$HOME/.config/xfce4/terminal/terminalrc" ]
 }
