2f72446aef2839f4ab8c1ee64503808e7e2e092d
Bastien Barbé--Creuly <bastien.barbe.creuly@gmail.com>
2018-10-10 23:46:44 +0200

    fix cargo/rustc autoinstall

--- plugins/rust.zsh
+++ plugins/rust.zsh
@@ -7,6 +7,6 @@ function has_rust() {
 
 function install_rust() {
 	if ! has_rust; then
-		curl https://sh.rustup.rs -sSf | sh > /dev/null
+		curl https://sh.rustup.rs -sSf | sh -s -- -y > /dev/null
 	fi
 }
