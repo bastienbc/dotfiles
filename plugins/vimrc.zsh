e4722cdc8a66ef386c05f5766e09dcceb05f321e
Bastien Barbé Creuly <bastien.barbe-creuly-externe@acoss.fr>
2018-11-30 14:11:48 +0100

    Add personnal vim snippets; Add command to change tmux yank selection

--- plugins/vimrc.zsh
+++ plugins/vimrc.zsh
@@ -4,3 +4,9 @@ if [ ! -f "${HOME}/.vimrc" ]
 then
 	ln -s "${0:A:h:h}/.vimrc" "$HOME/.vimrc" >& /dev/null
 fi
+
+if [ ! -f "${HOME}/.vim/UltiSnips/spec.snippets" ]
+then
+	mkdir -p "$HOME/.vim/UltiSnips" >& /dev/null
+	ln -s "${0:A:h:h}/spec.snippets" "$HOME/.vim/UltiSnips" >& /dev/null
+fi
