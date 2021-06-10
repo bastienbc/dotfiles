20a01a54fd25ed779b522b5005634f55c51be375
Bastien BC <bastien.barbe.creuly+git@gmail.com>
2021-06-11 00:13:12 +0200

    Use sheldon to install plugins

--- plugins/settings.zsh
+++ plugins/settings.zsh
@@ -3,3 +3,12 @@
 export BAT_THEME='Solarized (dark)'
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export WATCH_COMMAND='zsh -c'
+
+setopt AUTO_NAME_DIRS
+setopt CDABLE_VARS
+setopt HIST_FIND_NO_DUPS
+
+export GOPATH="${HOME}/.go:${GOPATH}"
+export GHQ_ROOT="${HOME}/.ghq"
+export PATH="${PATH}:${HOME}/.cargo/bin:${HOME}/.go/bin:${HOME}/.local/bin:${HOME}/bin"
+
